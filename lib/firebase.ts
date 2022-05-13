9ddb7d08cba3f948f828261df51a5d33c2d00b12
Vitor Pouza <vhnpouza@gmail.com>
2022-05-13 00:49:30 -0300

    admin page

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -47,3 +47,4 @@ export function postToJSON(doc) {
 }
 
 export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
